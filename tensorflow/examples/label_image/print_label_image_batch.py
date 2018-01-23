7c152432d297d5c779d6774bd81b7906b3a1e056
Brennan Christensen <brennan@orbotix.com>
2018-01-23 13:49:20 -0700

    print_label_image_batch.py now provides the labelling results csv name.

--- tensorflow/examples/label_image/print_label_image_batch.py
+++ tensorflow/examples/label_image/print_label_image_batch.py
@@ -1,14 +1,24 @@
 import os
 import argparse
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--image_dir",
         type=str
     )
-    image_dir = parser.parse_args().image_dir
+    parser.add_argument(
+        "--csv_suffix",
+        type=str,
+        default='-label_results.csv'
+    )
+    args = parser.parse_args()
+    image_dir = args.image_dir
+    csv_suffix = args.csv_suffix
+
+    category = image_dir.rstrip('/')
+
+    csv_name = category + csv_suffix
 
     for root, dirs, files in os.walk(image_dir):
         for file in files:
@@ -17,4 +27,6 @@ if __name__ == "__main__":
                           '--labels=/tmp/output_labels.txt --input_layer=Mul ' +
                           '--output_layer=final_result --input_mean=128 ' +
                           '--input_std=128 --image=' + image_dir + file +
-                          ' --category=' + image_dir)
+                          ' --csv_name=' + csv_name)
+
+    os.system('python squish_results_csv.py --category_count=7 --csv_file_path=' + csv_name)
